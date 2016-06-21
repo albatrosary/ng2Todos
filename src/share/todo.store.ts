a61e1dcde8cdcb87680dd32717de75fd4cef08a3
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-06-21 23:55:19 +0900

    change todolist

--- src/share/todo.store.ts
+++ src/share/todo.store.ts
@@ -4,17 +4,17 @@ import {TODO_STORE} from './todo';
 
 @Injectable()
 export class TodoStore {
-  private todolist: TODO_STORE[] = [];
+  private todos: TODO_STORE[] = [];
   
   public delete(index: number): void {
-    this.todolist.splice(index, 1);
+    this.todos.splice(index, 1);
   }
   
   public add(todo: TODO_STORE): void {
-    this.todolist.push(todo);
+    this.todos.push(todo);
   }
   
   public get list(): TODO_STORE[] {
-    return this.todolist;
+    return this.todos;
   }
 }
